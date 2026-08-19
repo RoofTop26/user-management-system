a9cf3f6ba0ba6ee283515c73adbefd986171eb05
Nguyễn Kim Hiếu <sandpassed15@gmail.com>
2026-08-19 20:50:31 +0700

    feature/week7-day2-bcrypt-service

---
+++ src/main/java/com/example/user_management_system/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.example.user_management_system.controller;
+
+import com.example.user_management_system.dto.AdminRequest;
+import com.example.user_management_system.entity.Admin;
+import com.example.user_management_system.service.AdminService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admins")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Admin> createAdmin(@RequestBody AdminRequest request) {
+        Admin created = adminService.createAdmin(request.getUsername(), request.getPassword());
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+}
